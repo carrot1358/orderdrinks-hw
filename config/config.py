a531ca8318fa182370950424cccefd633673ff5f
nattapad sa <nattapad.sa@rmuti.ac.th>
2024-09-24 01:37:46 +0700

    fix config

--- config/config.py
+++ config/config.py
@@ -6,6 +6,7 @@ with open('config.yaml', 'r') as config_file:
     config = yaml.safe_load(config_file)
 
 test_model = config["Test_Model"]
+test_camera = config["Test_Camera"]
 backEnd_ip = config["backEnd_ip"]
 backEnd_Port = config["backEnd_Port"]
 device_id = config["device_id"]
@@ -19,3 +20,4 @@ Roboflow_project = config["Roboflow_project"]
 Roboflow_version = config["Roboflow_version"]
 confidence_threshold = config["confidence_threshold"]
 debug = config["debug"]
+websocket_url = config["websocket_url"]
