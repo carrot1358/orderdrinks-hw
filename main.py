1dc570715e72b53b8b84b88038e1474caa336b15
nattapad sa <nattapad.sa@rmuti.ac.th>
2024-09-24 01:38:55 +0700

    make test_detection can capture image from camera

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from config import config
 from utils.detection_utils import create_detection_handler
 from utils.websocket_utils import create_websocket_handler
 from utils.gps_utils import GPS
+from picamera2 import Picamera2
 import requests
 import numpy as np
 import json
+import os
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -29,11 +31,33 @@ def run_detection_loop(detection_handler, ws_handler):
 
     cv2.destroyAllWindows()
 
-def test_detection(detection_handler, ws_handler):
-    # ตรวจจับด้วยรูปภาพทดสอบ
-    image_path = "./image/test_image/Test_image.jpg"
-    image = cv2.imread(image_path)
-    detection_handler.Detect_test(image)
+def test_detection(detection_handler, ws_handler, capture_from_camera=False):
+    if not capture_from_camera:
+        image_path = "./image/test_image/Test_image.jpg"
+        image = cv2.imread(image_path)
+    else:
+        image = detection_handler.get_image()
+    
+    # ทำการตรวจจับและรับผลลัพธ์
+    result = detection_handler.perform_detection(image=image)
+    if result:
+        # ส่งข้อมูลไปยัง WebSocket
+        ws_handler.send_bottle_result(result, sendto="both")
+        
+        # แสดงผลลัพธ์
+        logging.info(f"ผลการตรวจจับ: จำนวนขวด {result['bottle_count']}")
+        logging.info(f"ผลการตรวจจับ: จำนวนขวดที่มีน้ำ {result['has_water']}")
+        logging.info(f"ผลการตรวจจับ: จำนวนขวดที่ไม่มีน้ำ {result['no_water']}")
+    else:
+        logging.error("ไม่สามารถทำการตรวจจับได้")
+
+    # แสดงภาพผลลัพธ์ (ถ้ามี)
+    output_image_path = 'image/output/output_marked.png'
+    if os.path.exists(output_image_path):
+        output_image = cv2.imread(output_image_path)
+        cv2.imshow("ผลการตรวจจับ", output_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 def main():
     """
@@ -46,7 +70,7 @@ def main():
 
     if config.test_model:
         logging.info("ทำการตรวจจับด้วยรูปภาพทดสอบ")
-        test_detection(detection_handler, ws_handler)
+        test_detection(detection_handler, ws_handler, config.test_camera)
     else:
         logging.info("เริ่ม GPS")
         gps.start()
